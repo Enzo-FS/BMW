607c903ba92f5191065c5c3b94ac316296fc99f0
Enzo-FS <7enzoo7@gmail.com>
2025-10-01 16:29:04 -0300

    Add files via upload

---
+++ src/java.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const but = document.querySelector(".teste");
+const bloco1 = document.querySelector(".bloco1");
+
+bloco1.addEventListener('click', function(){
+    alert("oi");
+})
